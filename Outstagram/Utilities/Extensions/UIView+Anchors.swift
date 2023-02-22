baee54e7e6caf3cce753398cd45a9fdafdbb3f08
Anthony Petrov <beavean@gmail.com>
2023-02-22 14:58:05 +0200

    feat: Add UIView extension with layout constraints helper methods

---
+++ Outstagram/Utilities/Extensions/UIView+Anchors.swift
@@ -0,0 +1,220 @@
+//
+//  UIView+Anchors.swift
+//  Outstagram
+//
+//  Created by Beavean on 22.02.2023.
+//
+
+import UIKit
+
+public struct AnchoredConstraints {
+    public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
+}
+
+public extension UIView {
+    internal func anchor(top: NSLayoutYAxisAnchor? = nil,
+                         left: NSLayoutXAxisAnchor? = nil,
+                         bottom: NSLayoutYAxisAnchor? = nil,
+                         right: NSLayoutXAxisAnchor? = nil,
+                         paddingTop: CGFloat = 0,
+                         paddingLeft: CGFloat = 0,
+                         paddingBottom: CGFloat = 0,
+                         paddingRight: CGFloat = 0,
+                         width: CGFloat? = nil,
+                         height: CGFloat? = nil) {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if let top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+
+        if let left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+
+        if let bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+
+        if let right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+
+        if let width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+
+        if let height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+
+    internal func centerX(inView view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+
+    internal func centerY(inView view: UIView, leftAnchor: NSLayoutXAxisAnchor? = nil,
+                          paddingLeft: CGFloat = 0, constant: CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant).isActive = true
+
+        if let leftAnchor {
+            anchor(left: leftAnchor, paddingLeft: paddingLeft)
+        }
+    }
+
+    internal func setDimensions(height: CGFloat, width: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+
+    @discardableResult
+    func anchor(top: NSLayoutYAxisAnchor?,
+                leading: NSLayoutXAxisAnchor?,
+                bottom: NSLayoutYAxisAnchor?,
+                trailing: NSLayoutXAxisAnchor?,
+                padding: UIEdgeInsets = .zero,
+                size: CGSize = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+
+        if let top {
+            anchoredConstraints.top = topAnchor.constraint(equalTo: top, constant: padding.top)
+        }
+
+        if let leading {
+            anchoredConstraints.leading = leadingAnchor.constraint(equalTo: leading, constant: padding.left)
+        }
+
+        if let bottom {
+            anchoredConstraints.bottom = bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom)
+        }
+
+        if let trailing {
+            anchoredConstraints.trailing = trailingAnchor.constraint(equalTo: trailing, constant: -padding.right)
+        }
+
+        if size.width != 0 {
+            anchoredConstraints.width = widthAnchor.constraint(equalToConstant: size.width)
+        }
+
+        if size.height != 0 {
+            anchoredConstraints.height = heightAnchor.constraint(equalToConstant: size.height)
+        }
+
+        [anchoredConstraints.top,
+         anchoredConstraints.leading,
+         anchoredConstraints.bottom,
+         anchoredConstraints.trailing,
+         anchoredConstraints.width,
+         anchoredConstraints.height].forEach { $0?.isActive = true }
+
+        return anchoredConstraints
+    }
+
+    @discardableResult
+    func fillSuperview(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        let anchoredConstraints = AnchoredConstraints()
+        guard let superviewTopAnchor = superview?.topAnchor,
+              let superviewBottomAnchor = superview?.bottomAnchor,
+              let superviewLeadingAnchor = superview?.leadingAnchor,
+              let superviewTrailingAnchor = superview?.trailingAnchor
+        else {
+            return anchoredConstraints
+        }
+
+        return anchor(top: superviewTopAnchor,
+                      leading: superviewLeadingAnchor,
+                      bottom: superviewBottomAnchor,
+                      trailing: superviewTrailingAnchor,
+                      padding: padding)
+    }
+
+    @discardableResult
+    func fillSuperviewSafeAreaLayoutGuide(padding: UIEdgeInsets = .zero) -> AnchoredConstraints {
+        let anchoredConstraints = AnchoredConstraints()
+        if #available(iOS 11.0, *) {
+            guard let superviewTopAnchor = superview?.safeAreaLayoutGuide.topAnchor,
+                  let superviewBottomAnchor = superview?.safeAreaLayoutGuide.bottomAnchor,
+                  let superviewLeadingAnchor = superview?.safeAreaLayoutGuide.leadingAnchor,
+                  let superviewTrailingAnchor = superview?.safeAreaLayoutGuide.trailingAnchor
+            else {
+                return anchoredConstraints
+            }
+            return anchor(top: superviewTopAnchor,
+                          leading: superviewLeadingAnchor,
+                          bottom: superviewBottomAnchor,
+                          trailing: superviewTrailingAnchor,
+                          padding: padding)
+
+        } else {
+            return anchoredConstraints
+        }
+    }
+
+    func centerInSuperview(size: CGSize = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+
+    func centerXToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+    }
+
+    func centerYToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+    }
+
+    @discardableResult
+    func constrainHeight(_ constant: CGFloat) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        anchoredConstraints.height = heightAnchor.constraint(equalToConstant: constant)
+        anchoredConstraints.height?.isActive = true
+        return anchoredConstraints
+    }
+
+    @discardableResult
+    func constrainWidth(_ constant: CGFloat) -> AnchoredConstraints {
+        translatesAutoresizingMaskIntoConstraints = false
+        var anchoredConstraints = AnchoredConstraints()
+        anchoredConstraints.width = widthAnchor.constraint(equalToConstant: constant)
+        anchoredConstraints.width?.isActive = true
+        return anchoredConstraints
+    }
+
+    func setupShadow(opacity: Float = 0, radius: CGFloat = 0, offset: CGSize = .zero, color: UIColor = .black) {
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = radius
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+    }
+
+    convenience init(backgroundColor: UIColor = .clear) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+}
---
+++ Outstagram/Views/FeedCell.swift
@@ -0,0 +1,9 @@
+//
+//  FeedCell.swift
+//  Outstagram
+//
+//  Created by Beavean on 21.02.2023.
+//
+
+import Foundation
+
